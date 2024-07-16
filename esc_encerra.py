bab0999cd59eca7f3ce779921de3e98d0e59ed3c
Ivan Bragante <ivandelucca123@gmail.com>
2024-07-16 16:12:30 -0300

    Excluindo todos os arquivos do repositório

--- esc_encerra.py
+++
@@ -1,6 +0,0 @@
-import keyboard
-
-def check_for_esc():
-            if keyboard.is_pressed('esc'):
-                print("Execução interrompida pelo usuário.")
-                raise KeyboardInterrupt
